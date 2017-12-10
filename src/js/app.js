e004a5d7b9c4799171b94d755501869ef5689597
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-10 15:04:10 +0100

    Add time taken per star chart

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,91 @@
 (function(aoc) {
+    function hexToRGB(hex, alpha) {
+        // By @AJFarkas, from https://stackoverflow.com/a/28056903/419956
+
+        var r = parseInt(hex.slice(1, 3), 16),
+            g = parseInt(hex.slice(3, 5), 16),
+            b = parseInt(hex.slice(5, 7), 16);
+    
+        if (alpha) {
+            return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+        } else {
+            return "rgb(" + r + ", " + g + ", " + b + ")";
+        }
+    }
+
     class App {
         constructor(dal) {
             dal.getLeaderboardJson()
                 .then(data => this.loadStarsOverTime(data))
-                .then(data => this.loadPointsOverTime(data));
+                .then(data => this.loadPointsOverTime(data))
+                .then(data => this.loadTimePerStar(data));
+        }
+
+        loadTimePerStar(data) {
+            let datasets = [];
+            let n = Math.min(3, data.members.length);
+            let relevantMembers = data.members.sort((a,b) => b.score - a.score).slice(0,n);
+
+            for (let member of relevantMembers) {
+                let star1DataSet = {
+                    label: `${member.name} (★)`,
+                    stack: `Stack ${member.name}`,
+                    backgroundColor: member.color,
+                    data: [],
+                };
+
+                let star2DataSet = {
+                    label: `${member.name} (★★)`,
+                    stack: `Stack ${member.name}`,
+                    backgroundColor: hexToRGB(member.color, 0.7),
+                    data: [],
+                };
+
+                for (let i = 1; i <= 25; i++) {
+                    let star1 = data.stars.find(s => s.memberId === member.id && s.dayNr === i && s.starKey === "1");
+                    let star2 = data.stars.find(s => s.memberId === member.id && s.dayNr === i && s.starKey === "2");
+
+                    star1DataSet.data.push(!!star1 ? star1.timeTaken : 0);
+                    star2DataSet.data.push(!!star2 ? star2.timeTaken - star1.timeTaken : 0);
+                }
+
+                // Workaround for bug with "logarithmic" axes: https://github.com/chartjs/Chart.js/issues/4934
+                star1DataSet.data = star1DataSet.data.map(x => Math.log10(x));
+                star2DataSet.data = star2DataSet.data.map(x => Math.log10(x));
+                
+                datasets.push(star1DataSet);
+                datasets.push(star2DataSet);
+            }
+
+
+            let pointsOverTimeChart = new Chart(document.getElementById("timePerStar").getContext("2d"), {
+                type: "bar",
+                data: {
+                    labels: _.range(1, 26),
+                    datasets: datasets,
+                },
+                options: {
+                    responsive: true,
+                    legend: {
+                        position: "left",
+                    },
+                    title: {
+                        display: true,
+                        text: `Log10(minutes) taken per star (top ${n} players)`,
+                        fontSize: 24,
+                    },
+                    scales: {
+                        xAxes: [{
+                            stacked: true,
+                        }],
+                        yAxes: [{
+                            stacked: true,
+                        }],
+                    }
+                }
+            });
+
+            return data;
         }
 
         loadPointsOverTime(data) {
@@ -22,7 +104,7 @@
                         }
                     })
                 };
-            });            
+            });
 
             let pointsOverTimeChart = new Chart(document.getElementById("pointsOverTime").getContext("2d"), {
                 type: "line",
@@ -30,7 +112,7 @@
                     datasets: datasets,
                 },
                 options: {
-                    responsive: false,
+                    responsive: true,
                     legend: {
                         position: "left",
                     },
@@ -43,20 +125,22 @@
                         xAxes: [{
                             type: "time",
                             time: {
+                                min: moment([2017,10,30,5,0,0]),
                                 max: data.maxMoment,
-                                unit: "day", 
+                                unit: "day",
                                 stepSize: 1,
+                                displayFormats: { day: "D" },
                             },
                         }],
                         yAxes: [{
-                            ticks: {
-                                stepSize: 5,
+                            ticks: {                               
+                                min: 0,
                             },
                         }],
                     }
                 }
             });
-            
+
             return data;
         }
 
@@ -84,7 +168,7 @@
                     datasets: datasets,
                 },
                 options: {
-                    responsive: false,
+                    responsive: true,
                     legend: {
                         position: "left",
                     },
@@ -97,14 +181,17 @@
                         xAxes: [{
                             type: "time",
                             time: {
+                                min: moment([2017,10,30,5,0,0]),
                                 max: data.maxMoment,
-                                unit: "day", 
+                                unit: "day",
                                 stepSize: 1,
+                                displayFormats: { day: "D" },
                             }
                         }],
                         yAxes: [{
                             ticks: {
                                 stepSize: 1,
+                                min: 0,
                             },
                         }],
                     }
--- src/js/dal.js
+++ src/js/dal.js
@@ -1,10 +1,73 @@
 (function (aoc) {
     const dummyData = {
+        "event": "2017",
         "members": {
+            "123456": {
+                "global_score": 0,
+                "local_score": 187,
+                "last_star_ts": "2017-12-07T03:15:45-0500",
+                "completion_day_level": {
+                    "1": {
+                        "1": {
+                            "get_star_ts": "2017-12-02T04:10:47-0500"
+                        },
+                        "2": {
+                            "get_star_ts": "2017-12-02T05:21:43-0500"
+                        }
+                    },
+                    "2": {
+                        "1": {
+                            "get_star_ts": "2017-12-03T04:28:10-0500"
+                        },
+                        "2": {
+                            "get_star_ts": "2017-12-03T11:13:36-0500"
+                        }
+                    },
+                    "3": {
+                        "1": {
+                            "get_star_ts": "2017-12-03T12:53:40-0500"
+                        },
+                        "2": {
+                            "get_star_ts": "2017-12-05T03:50:53-0500"
+                        }
+                    },
+                    "4": {
+                        "1": {
+                            "get_star_ts": "2017-12-04T01:15:43-0500"
+                        },
+                        "2": {
+                            "get_star_ts": "2017-12-04T01:34:34-0500"
+                        }
+                    },
+                    "5": {
+                        "1": {
+                            "get_star_ts": "2017-12-05T01:22:29-0500"
+                        },
+                        "2": {
+                            "get_star_ts": "2017-12-05T01:26:38-0500"
+                        }
+                    },
+                    "6": {
+                        "1": {
+                            "get_star_ts": "2017-12-06T14:43:37-0500"
+                        },
+                        "2": {
+                            "get_star_ts": "2017-12-06T15:41:32-0500"
+                        }
+                    },
+                    "7": {
+                        "1": {
+                            "get_star_ts": "2017-12-07T03:15:45-0500"
+                        }
+                    }
+                },
+                "id": "123456",
+                "stars": 13,
+                "name": "John Doe"
+            },
             "190664": {
+                "local_score": 20,
                 "name": "Jeroen Heijmans",
-                "local_score": 18,
-                "last_star_ts": "2017-12-09T01:45:41-0500",
                 "completion_day_level": {
                     "1": {
                         "1": {
@@ -77,15 +140,23 @@
                         "2": {
                             "get_star_ts": "2017-12-09T01:45:41-0500"
                         }
+                    },
+                    "10": {
+                        "1": {
+                            "get_star_ts": "2017-12-10T01:20:29-0500"
+                        },
+                        "2": {
+                            "get_star_ts": "2017-12-10T02:26:26-0500"
+                        }
                     }
                 },
-                "stars": 18,
-                "global_score": 0,
-                "id": "190664"
+                "id": "190664",
+                "last_star_ts": "2017-12-10T02:26:26-0500",
+                "stars": 20,
+                "global_score": 0
             }
         },
-        "owner_id": "190664",
-        "event": "2017"
+        "owner_id": "190664"
     };
 
     const starSorter = (a, b) => a.getStarTimestamp.localeCompare(b.getStarTimestamp);
@@ -103,21 +174,33 @@
 
                             for (let dayKey of Object.keys(m.completion_day_level)) {
                                 for (let starKey of Object.keys(m.completion_day_level[dayKey])) {
+                                    let starMoment = moment(m.completion_day_level[dayKey][starKey].get_star_ts).utc();
+
                                     let star = {
                                         memberId: m.id,
-                                        day: dayKey,
+                                        dayNr: parseInt(dayKey, 10),
+                                        dayKey: dayKey,
+                                        starNr: parseInt(starKey, 10),
                                         starKey: starKey,
                                         getStarDay: parseInt(`${dayKey}.${starKey}`, 10),
                                         getStarTimestamp: m.completion_day_level[dayKey][starKey].get_star_ts,
-                                        getStarMoment: moment(m.completion_day_level[dayKey][starKey].get_star_ts),
+                                        getStarMoment: starMoment,
+                                        timeTaken: null, // adding this later on, which is easier :D
                                     };
+
                                     stars.push(star);
                                     m.stars.push(star);
                                 }
                             }
 
                             m.stars = m.stars.sort(starSorter);
-                            m.stars.forEach((s, idx) => s.nrOfStarsAfterThisOne = idx + 1);
+
+                            m.stars.forEach((s, idx) => {
+                                s.nrOfStarsAfterThisOne = idx + 1;
+
+                                let startOfDay = moment.utc([2017, 11, s.dayNr, 5, 0, 0]); // AoC starts at 05:00 UTC
+                                s.timeTaken = s.getStarMoment.diff(startOfDay, "minutes");
+                            });
 
                             return m;
                         })
@@ -127,7 +210,7 @@
                     let colors = palette('tol-rainbow', members.length).map(c => `#${c}`);
                     members.forEach((m, idx) => m.color = colors[idx]);
 
-                    let allMoments = stars.map(s => s.getStarMoment).concat([moment("2017-12-05T00:00:00-0000")]);
+                    let allMoments = stars.map(s => s.getStarMoment).concat([moment("2017-12-25T00:00:00-0000")]);
                     let maxMoment = moment.min([moment.max(allMoments), moment("2017-12-31T00:00:00-0000")]);
 
                     let availablePoints = {};
@@ -144,7 +227,7 @@
                     let rawDataSets = {};
 
                     for (let star of orderedStars) {
-                        star.points = availablePoints[star.day][star.starKey]--;
+                        star.points = availablePoints[star.dayKey][star.starKey]--;
                     }
 
                     for (let m of members) {
@@ -152,6 +235,7 @@
                         for (let s of m.stars.sort(starSorter)) {
                             accumulatedPoints += s.points;
                             s.nrOfPointsAfterThisOne = accumulatedPoints;
+                            m.score = accumulatedPoints;
                         }
                     }
 
