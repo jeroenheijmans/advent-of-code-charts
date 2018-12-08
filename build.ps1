6ea4669fe5588629e110db055313b9c772ba8331
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-12-08 20:24:28 +0100

    Change setup for Firefox plugin
    Fixes #14

--- build.ps1
+++ build.ps1
@@ -6,3 +6,7 @@ Copy-Item node_modules/moment/min/moment.min.js build/
 Copy-Item node_modules/chart.js/dist/Chart.min.js build/
 Copy-Item src/js/app.js build/app.js
 Copy-Item manifest.json build/manifest.json
+
+# Workaround for: https://stackoverflow.com/questions/51948350/
+# See also: https://github.com/chartjs/Chart.js/issues/5901
+Add-Content -Path build/moment.min.js -Value "`n`nwindow.moment = moment;"
--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@ rm -rf build
 mkdir build
 
 cp icon*.png build/
-cp node_modules/moment/min/moment.min.js build/
-cp node_modules/chart.js/dist/Chart.min.js build/
+cp node_modules/moment/min/moment.min.js build/moment.min.js
+cp node_modules/chart.js/dist/Chart.min.js build/Chart.min.js
 cp src/js/app.js build/app.js
 cp manifest.json build/manifest.json
+
+# Workaround for: https://stackoverflow.com/questions/51948350/
+# See also: https://github.com/chartjs/Chart.js/issues/5901
+echo "; window.moment = moment;" >> build/moment.min.js
