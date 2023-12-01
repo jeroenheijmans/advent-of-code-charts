adf1b1e243ef3a56d0cd7343e4401ca3a8de132d
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-12-01 20:20:19 +0100

    Add extra test case for easier testing

---
+++ src/js/dummyData-oneDay.js
@@ -0,0 +1,147 @@
+(function (aoc) {
+  aoc["dummyData"] = {
+    members: {
+      12345: {
+        last_star_ts: 0,
+        local_score: 0,
+        completion_day_level: {},
+        stars: 0,
+        name: "Senor Wontplaythisyear",
+        global_score: 0,
+        id: 12345,
+      },
+      23456: {
+        name: "Felt Welkore",
+        global_score: 0,
+        id: 23456,
+        stars: 2,
+        last_star_ts: 1701407764,
+        local_score: 81,
+        completion_day_level: {
+          1: {
+            1: {
+              star_index: 775,
+              get_star_ts: 1701407311,
+            },
+            2: {
+              get_star_ts: 1701407764,
+              star_index: 4735,
+            },
+          },
+        },
+      },
+      34567: {
+        name: "Sté der Bres",
+        global_score: 0,
+        id: 34567,
+        stars: 0,
+        local_score: 0,
+        completion_day_level: {},
+        last_star_ts: 0,
+      },
+      45678: {
+        local_score: 0,
+        completion_day_level: {},
+        last_star_ts: 0,
+        global_score: 0,
+        name: "mcguffin",
+        id: 45678,
+        stars: 0,
+      },
+      56789: {
+        completion_day_level: {
+          1: {
+            1: {
+              star_index: 0,
+              get_star_ts: 1701407237,
+            },
+            2: {
+              star_index: 12083,
+              get_star_ts: 1701409132,
+            },
+          },
+        },
+        local_score: 81,
+        last_star_ts: 1701409132,
+        global_score: 0,
+        id: 56789,
+        name: "Jeroen Heijmans",
+        stars: 2,
+      },
+      98765: {
+        completion_day_level: {
+          1: {
+            1: {
+              star_index: 58850,
+              get_star_ts: 1701422979,
+            },
+          },
+        },
+        local_score: 34,
+        last_star_ts: 1701422979,
+        name: "Martha G.",
+        global_score: 0,
+        id: 98765,
+        stars: 1,
+      },
+      65432: {
+        local_score: 58,
+        completion_day_level: {
+          1: {
+            1: {
+              get_star_ts: 1701444654,
+              star_index: 138452,
+            },
+            2: {
+              star_index: 142448,
+              get_star_ts: 1701445648,
+            },
+          },
+        },
+        last_star_ts: 1701445648,
+        global_score: 0,
+        id: 65432,
+        name: "pittar93",
+        stars: 2,
+      },
+      54321: {
+        stars: 1,
+        id: 54321,
+        global_score: 0,
+        name: "CapnCr00k",
+        completion_day_level: {
+          1: {
+            1: {
+              get_star_ts: 1701423684,
+              star_index: 61614,
+            },
+          },
+        },
+        local_score: 33,
+        last_star_ts: 1701423684,
+      },
+      11223: {
+        name: "Acrill West",
+        global_score: 0,
+        id: 11223,
+        stars: 2,
+        last_star_ts: 1701420716,
+        local_score: 76,
+        completion_day_level: {
+          1: {
+            1: {
+              get_star_ts: 1701418276,
+              star_index: 40795,
+            },
+            2: {
+              get_star_ts: 1701420716,
+              star_index: 49809,
+            },
+          },
+        },
+      },
+    },
+    owner_id: 366773,
+    event: "2023",
+  };
+})((window.aoc = window.aoc || {}));
